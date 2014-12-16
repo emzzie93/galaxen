5abb7103ca01f74221898b995adba76d7a407e65
matos000 <matos000@student.liu.se>
2014-12-16 20:06:07 +0100

    menysetup

---
+++ Menysetup.cpp
@@ -0,0 +1,59 @@
+#include "Menysetup.h"
+
+
+
+Menysetup::Menysetup(int xpos, int ypos, int width, int height)
+{
+    posMeny.x = xpos;
+    posMeny.y = ypos;
+    posMeny.w = width;
+    posMeny.h = height;
+}
+
+
+
+
+void Menysetup::CreateMeny(GameSetup World)
+ {
+
+Menyheader = SDL_LoadBMP("Asteroids.bmp");
+	if (Menyheader == NULL)
+	{
+        std::cout << "Error in loading Menyheader." << SDL_GetError() << std::endl;
+		SDL_Quit();
+	}
+headermeny = SDL_CreateTextureFromSurface(World.GameRender, Menyheader);
+
+SDL_FreeSurface(Menyheader);
+
+Newgame = SDL_LoadBMP("play.bmp");
+	if (Newgame == NULL)
+	{
+        std::cout << "Error in loading newgame." << SDL_GetError() << std::endl;
+		SDL_Quit();
+	}
+newgame = SDL_CreateTextureFromSurface(World.GameRender, Newgame);
+SDL_FreeSurface(Newgame);
+
+ Instructions = SDL_LoadBMP("about.bmp");
+	if (Instructions == NULL)
+	{
+        std::cout << "Error in loading Instructions." << SDL_GetError() << std::endl;
+		SDL_Quit();
+	}
+instructions = SDL_CreateTextureFromSurface(World.GameRender, Instructions);
+SDL_FreeSurface(Instructions);
+
+Quitgame = SDL_LoadBMP("exit.bmp");
+	if (Quitgame == NULL)
+	{
+        std::cout << "Error in loading Quitgame." << SDL_GetError() << std::endl;
+		SDL_Quit();
+	}
+quitgame = SDL_CreateTextureFromSurface(World.GameRender, Quitgame);
+SDL_FreeSurface(Quitgame);
+
+
+}
+
+
